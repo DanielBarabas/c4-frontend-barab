3d3e29258172ee46bb4f199433f5cc70593f7667
Szabi <molnar.sz99@gmail.com>
2024-04-20 16:31:00 +0200

    first try

---
+++ Dropdown.svelte
@@ -0,0 +1,12 @@
+<!-- Dropdown.svelte -->
+<script>
+  export let options;
+  export let selectedOption;
+  export let handleChange;
+</script>
+
+<select value={selectedOption} on:change={handleChange}>
+  {#each options as option}
+    <option value={option}>{option}</option>
+  {/each}
+</select>
