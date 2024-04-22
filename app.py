bc64557029ee11a6c2c47771b9e055c08313c96c
DanielBarabas <barabas.daniel1@gmail.com>
2024-04-22 20:00:47 +0200

    minor formatting

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import pandas as pd
 import random
 import query
 
-
-
 st.title("Content Rating")
 
 # Define the options for the categories and severities
@@ -28,8 +26,13 @@ criteria_2_severity = st.selectbox("Select Severity 2:", severity_options)
 
 # This part will be done by the back-end:
 
-criteria_list = [criteria_1_category,criteria_1_severity,criteria_2_category,criteria_2_severity]
-# criteria_list = [criteria_1_category,criteria_1_severity,criteria_2_category,criteria_2_severity]
+# criteria_list = ['Sex & Nudity','Severe','Profanity','Moderate']
+criteria_list = [
+    criteria_1_category,
+    criteria_1_severity,
+    criteria_2_category,
+    criteria_2_severity,
+]
 st.write("Recommended Show:", query.query_series(criteria_list=criteria_list))
 
 
